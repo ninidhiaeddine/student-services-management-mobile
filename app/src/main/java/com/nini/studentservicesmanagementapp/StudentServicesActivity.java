7115eaa3ba45cec81f5afb5fa95cacabaffd2bea
Dhia Eddine Nini <dxn00@mail.aub.edu>
2022-11-06 16:59:07 +0200

    Added toolbar to Student Services Screen

--- app/src/main/java/com/nini/studentservicesmanagementapp/StudentServicesActivity.java
+++ app/src/main/java/com/nini/studentservicesmanagementapp/StudentServicesActivity.java
@@ -1,14 +1,68 @@
 package com.nini.studentservicesmanagementapp;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class StudentServicesActivity extends AppCompatActivity {
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_services);
+
+        findViews();
+        setUpToolbar();
+        setUpNavigationView();
+    }
+
+    private void findViews() {
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navigation_view);
+        toolbar = findViewById(R.id.toolbar);
+    }
+
+    private void setUpToolbar() {
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationOnClickListener(menuItem -> drawerLayout.open());
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar == null)
+            return;
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    private void setUpNavigationView() {
+        navigationView.setNavigationItemSelectedListener(menuItem -> {
+            switch (menuItem.getItemId()) {
+                case R.id.reservations_item:
+                    // Do something here
+                    break;
+                case R.id.account_item:
+                    // Do something here
+                    break;
+                case R.id.help_item:
+                    // Do something here
+                    break;
+                case R.id.log_out_item:
+                    // Do something here
+                    break;
+                default:
+                    // Do nothing
+                    break;
+            }
+            return true;
+        });
     }
 }
