3e22574adb9209106e1f5c11b6c5c2b9a473ccf2
Dhia Eddine Nini <dxn00@mail.aub.edu>
2022-11-22 20:11:13 +0200

    Fixed multiple issues in existing layouts and added new screens (Dark Mode issue still pending)

--- app/src/main/java/com/nini/studentservicesmanagementapp/StudentBookingCalendarActivity.java
+++ app/src/main/java/com/nini/studentservicesmanagementapp/StudentBookingCalendarActivity.java
@@ -3,12 +3,29 @@ package com.nini.studentservicesmanagementapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CalendarView;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class StudentBookingCalendarActivity extends AppCompatActivity {
+    private CalendarView calendarView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_booking_calendar);
+
+        findViews();
+        setUpCalendarView();
+    }
+
+    private void findViews() {
+        calendarView = findViewById(R.id.calendarView);
+    }
+
+    private void setUpCalendarView() {
+        Date today = new Date();
+        calendarView.setMinDate(today.getTime());
     }
 }
