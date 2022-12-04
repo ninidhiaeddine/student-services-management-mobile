8c761a6ef07ad502712d9cd037daa3e86ad6a976
Dhia Eddine Nini <dxn00@mail.aub.edu>
2022-12-04 18:36:36 +0200

    Added authentication logic

--- app/src/main/java/com/nini/studentservicesmanagementapp/activities/StudentLoginActivity.java
+++ app/src/main/java/com/nini/studentservicesmanagementapp/activities/StudentLoginActivity.java
@@ -3,13 +3,21 @@ package com.nini.studentservicesmanagementapp.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.textfield.TextInputEditText;
 import com.nini.studentservicesmanagementapp.R;
+import com.nini.studentservicesmanagementapp.data.api.AuthApiService;
+import com.nini.studentservicesmanagementapp.data.api.VolleyCallback;
+import com.nini.studentservicesmanagementapp.data.dtos.SignInDto;
 
 public class StudentLoginActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
+
+    private TextInputEditText emailInputEditText;
+    private TextInputEditText passwordInputEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,19 +25,39 @@ public class StudentLoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_student_login);
     }
 
+    private void findViews() {
+        emailInputEditText = findViewById(R.id.emailInputEditText);
+        passwordInputEditText = findViewById(R.id.passwordInputEditText);
+    }
+
     public void loginOnClick(View view) {
-        // authentication logic goes here ...
+        // retrieve input email & password:
+        SignInDto dto = new SignInDto();
+        dto.email = emailInputEditText.getText().toString() + "@mail.aub.edu";
+        dto.password = passwordInputEditText.getText().toString();
 
-        // create extras and put necessary info:
-        Bundle extras = new Bundle();
-        extras.putInt("fragmentLayoutId", R.layout.fragment_student_services);
+        // call api:
+        AuthApiService apiService = new AuthApiService(this);
+        apiService.signInStudent(dto, new VolleyCallback() {
+            @Override
+            public void onSuccess(String response) {
+                // create extras and put necessary info:
+                Bundle extras = new Bundle();
+                extras.putInt("fragmentLayoutId", R.layout.fragment_student_services);
 
-        // prepare intent for new activity:
-        Intent intent = new Intent(StudentLoginActivity.this, ProfileToolbarActivity.class);
-        intent.putExtras(extras);
+                // prepare intent for new activity:
+                Intent intent = new Intent(StudentLoginActivity.this, ProfileToolbarActivity.class);
+                intent.putExtras(extras);
 
-        // start activity:
-        startActivity(intent);
+                // start activity:
+                startActivity(intent);
+            }
+
+            @Override
+            public void onError(String error) {
+                Toast.makeText(StudentLoginActivity.this, error, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     public void signUpOnClick(View view) {
