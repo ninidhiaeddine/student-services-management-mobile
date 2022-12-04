76351a4a5942d792537c294600f9823dbc92bb17
Dhia Eddine Nini <dxn00@mail.aub.edu>
2022-12-05 01:47:44 +0200

    Removed copying method from student

--- app/src/main/java/com/nini/studentservicesmanagementapp/data/models/Student.java
+++ app/src/main/java/com/nini/studentservicesmanagementapp/data/models/Student.java
@@ -1,7 +1,5 @@
 package com.nini.studentservicesmanagementapp.data.models;
 
-import androidx.annotation.NonNull;
-
 public class Student implements Cloneable {
     public int PK_Student;
     public String firstName;
@@ -10,23 +8,6 @@ public class Student implements Cloneable {
     public int studentId;
     public int gender;
     public int isDorms;
-    public String hashedPassword;
-
-    public static void copy(Student source, Student destination) {
-        destination = source.clone();
-    }
-
-    @NonNull
-    @Override
-    public Student clone() {
-        try {
-            Student clone = (Student) super.clone();
-            // TODO: copy mutable state here, so the clone can't change the internals of the original
-            return clone;
-        } catch (CloneNotSupportedException e) {
-            throw new AssertionError();
-        }
-    }
 
     public String getFullName() {
         return firstName + " " + lastName;
